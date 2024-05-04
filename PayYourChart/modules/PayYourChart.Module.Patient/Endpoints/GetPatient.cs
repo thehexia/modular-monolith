9a3b1f38b5017f3189228f5779c2969ca16517a1
Hoang Nguyen <hoangnguyen@Hoangs-MacBook-Pro.local>
2024-05-04 15:17:22 -0400

    Fix swagger docs on no content return

--- PayYourChart/modules/PayYourChart.Module.Patient/Endpoints/GetPatient.cs
+++ PayYourChart/modules/PayYourChart.Module.Patient/Endpoints/GetPatient.cs
@@ -1,43 +1,36 @@
-﻿using AutoMapper;
-using FastEndpoints;
+﻿using FastEndpoints;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 
 namespace PayYourChart.Module.Patient;
 
-[HttpGet($"{ApiPath.Base}/{{id}}")]
-[AllowAnonymous] // Temporarily allow this for testing
-[PostProcessor<GetPatientExceptionProcessor>]
 internal class GetPatient(IPatientRepository patient, IPatientDtoMapperFactory mapper) : Endpoint<GetPatientByIdRequest, PatientDto>
 {
     // I personally think its ok to skip the service if there is no business logic.
     readonly IPatientRepository _patient = patient;
     readonly IPatientDtoMapperFactory _mapper = mapper;
 
+    // We use Configure() instead of Attributes because we want to do custom swagger docs.
+    public override void Configure()
+    {
+        Get($"{ApiPath.Base}/{{id}}");
+        AllowAnonymous();
+        Description(b => b.Produces(204));
+    }
+
+
     public override async Task HandleAsync(GetPatientByIdRequest req, CancellationToken ct)
     {
-        await SendAsync(_mapper.Get().Map<PatientDto>(await _patient.GetByIdAsync(req.Id)));
-    }
-}
-
-
-internal class GetPatientExceptionProcessor : IPostProcessor<GetPatientByIdRequest, PatientDto>
-{
-    public async Task PostProcessAsync(IPostProcessorContext<GetPatientByIdRequest, PatientDto> ctx, CancellationToken ct = default)
-    {
-        if (ctx.Response == null) 
+        Patient? patient = await _patient.GetByIdAsync(req.Id);
+        if (patient != null)
         {
-            await ctx.HttpContext.Response.SendAsync(
-                new InternalErrorResponse
-                {
-                    Status = "No content",
-                    Code = 204,
-                    Reason = "No patient with the matching Id was found."
-                }, 204); 
+            await SendAsync(_mapper.Get().Map<PatientDto>(patient));
         }
-
-        if (!ctx.HasExceptionOccurred)
-            return;
-
-        ctx.ExceptionDispatchInfo.Throw();
+        else
+        {
+            await SendNoContentAsync();
+        }
     }
 }
+
+
