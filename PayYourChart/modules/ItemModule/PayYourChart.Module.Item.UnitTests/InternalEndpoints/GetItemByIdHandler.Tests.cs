edae07f39631f06aca64c4c02466377a77a26e51
Hoang Nguyen <hoangnguyen@Hoangs-MacBook-Pro.local>
2024-05-10 16:11:27 -0400

    Adding another unit test to getitem

--- PayYourChart/modules/ItemModule/PayYourChart.Module.Item.UnitTests/InternalEndpoints/GetItemByIdHandler.Tests.cs
+++ PayYourChart/modules/ItemModule/PayYourChart.Module.Item.UnitTests/InternalEndpoints/GetItemByIdHandler.Tests.cs
@@ -11,7 +11,7 @@ public class GetItemByIdHandlerTests(InternalEndpointHandlersTestFixture app) : 
     readonly InternalEndpointHandlersTestFixture _app = app;
 
     [Fact]
-    public async Task GetItemByIdHandler_returns_item_with_no_special_approval_in_description_if_price_below_10_000()
+    public async Task GetItemByIdHandler_returns_item_with_no_special_approval_if_price_below_10_000()
     {
         // Arrange
         const long itemId = 1;
@@ -31,5 +31,31 @@ public class GetItemByIdHandlerTests(InternalEndpointHandlersTestFixture app) : 
         Assert.True(result.IsSuccess);
         Assert.False(result.Value.SpecialApprovalRequired);
         Assert.Null(result.Value.SpecialApprovalReason);
+    }
+
+
+    [Theory]
+    [InlineData(10_000)]
+    [InlineData(11_000.05)]
+    public async Task GetItemByIdHandler_returns_item_with_special_approval_if_price_greater_or_equal_10_000(decimal price)
+    {
+        // Arrange
+        const long itemId = 1;
+        Item item = _app.f
+            .Build<Item>()
+            .With(item => item.Id, itemId)
+            .With(item => item.Price, price)
+            .Create();
+        
+        _app.ItemRepository.GetItemAsync(itemId).Returns(item);
+        GetItemByIdHandler sut = _app.f.Create<GetItemByIdHandler>();
+
+        // Act
+        Result<GetItemResponse> result = await sut.Handle(new(itemId));
+
+        // Assert
+        Assert.True(result.IsSuccess);
+        Assert.True(result.Value.SpecialApprovalRequired);
+        Assert.Equal("Item costs over $10,000", result.Value.SpecialApprovalReason);
     }
 }
